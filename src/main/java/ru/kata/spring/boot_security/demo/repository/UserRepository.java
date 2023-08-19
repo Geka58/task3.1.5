2d4bb8d2e2ab1699482b47150efb8edad7b46eef
Geka58 <he588@yandex.ru>
2023-08-20 00:54:00 +0300

    2

--- src/main/java/ru/kata/spring/boot_security/demo/repository/UserRepository.java
+++ src/main/java/ru/kata/spring/boot_security/demo/repository/UserRepository.java
@@ -11,6 +11,7 @@ public interface UserRepository extends JpaRepository<User, Integer> {
 
     @Query("Select u from User u left join fetch u.roles where u.email=:email")
     User findByEmail(String email);
+
     @Query("Select u from User u left join fetch u.roles where u.username=:username")
     User findByUsername(String username);
 
